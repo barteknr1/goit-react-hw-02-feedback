aafcfceb3819198d06fc4f842b362f25071274ed
Bartek <Barteknr1@gmail.com>
2023-05-26 13:12:33 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 import React, {Component} from "react";
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  };
+
   static defaultProps = {
     step: 1,
   };
+
+  static propTypes = {
+  };
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
-  }
+  };
+
   render() {
     return (
-  )
-  }
+      <>
+        <div>
+          <p>hello world!</p>
+      </div>
+      </>
+  );
+  };
 }
+
+export default App
